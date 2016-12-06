52b5258b348ab753fc3f70ec5699f1b4662a7924
Xenia Tay <xeniatzy@gmail.com>
2016-12-05 20:49:55 -0800

    Built files

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ var webpack = require('webpack');
 module.exports = {
   entry: './js/main.js',
   devtool: "source-map",
-  output: { path: __dirname, filename: 'bundle.js' },
+  output: { path: __dirname + '/build', filename: 'bundle.js' },
   module: {
     loaders: [
       {
