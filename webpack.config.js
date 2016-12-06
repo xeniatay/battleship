ef12cd0122a2493d599e1380148d9ea5231feaf3
Xenia Tay <xeniatzy@gmail.com>
2016-12-05 19:40:02 -0800

    Set up build process

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ var webpack = require('webpack');
 
 module.exports = {
   entry: './js/main.js',
+  devtool: "source-map",
   output: { path: __dirname, filename: 'bundle.js' },
   module: {
     loaders: [
@@ -11,9 +12,19 @@ module.exports = {
         loader: 'babel-loader',
         exclude: /node_modules/,
         query: {
-          presets: ['es2015', 'react']
+          presets: ['es2015', 'react', 'stage-0']
         }
+      },
+      {
+        test: /\.css/,
+        loaders: ['style-loader', 'css-loader'],
+        include: __dirname + '/css'
+      },
+      {
+        test: /\.scss$/,
+        loaders: ["style-loader", "css-loader", "sass-loader"],
+        include: __dirname + '/css'
       }
     ]
   },
-};
+}
